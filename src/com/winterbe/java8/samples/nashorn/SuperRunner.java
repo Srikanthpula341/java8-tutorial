a2ecbba3022163ec27ca4a144765daadb4720d3b
Srikanthpula341 <pulasrikanth1999@gmail.com>
2024-02-04 14:14:39 +0530

    fix

---
+++ src/com/winterbe/java8/samples/nashorn/SuperRunner.java
@@ -0,0 +1,13 @@
+package com.winterbe.java8.samples.nashorn;
+
+/**
+ * @author Srikanth Pula
+ */
+public class SuperRunner implements Runnable {
+
+    @Override
+    public void run() {
+        System.out.println("super run");
+    }
+
+}
